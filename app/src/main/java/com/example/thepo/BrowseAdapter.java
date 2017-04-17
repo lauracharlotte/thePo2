2368b6651d0e15e47aa8c5e887590964c382860f
BYUB <byub@byub-webloan-1.local>
2017-04-17 11:46:53 -0600

    in progress

--- app/src/main/java/com/example/thepo/BrowseAdapter.java
+++ app/src/main/java/com/example/thepo/BrowseAdapter.java
@@ -1,24 +1,28 @@
 package com.example.thepo;
 
 import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
-/**
- * Created by laurahovey on 3/27/17.
- */
 
 public class BrowseAdapter extends RecyclerView.Adapter<BrowseAdapter.BrowseHolder>{
 
     private List<BrowseItem> browseItem;
     private LayoutInflater inflater;
     int tracker = 0;
+
+//    Snackbar.make(findViewById(R.id.browseitem), "You're currently on the radio page!",
+//    Snackbar.LENGTH_SHORT).show();
+
+
 
     public BrowseAdapter(List<BrowseItem> browseItem, Context c)
     {
@@ -30,27 +34,34 @@ public class BrowseAdapter extends RecyclerView.Adapter<BrowseAdapter.BrowseHold
     @Override
     public BrowseHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = inflater.inflate(R.layout.browse_item, parent, false);
+//        view.setOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Snackbar.make(v.findViewById(R.id.browseitem), "clicked" + getAdapterPosition(), Snackbar.LENGTH_SHORT).show();
+//                    }
+//                }
+//        );
         return new BrowseHolder(view);
     }
 
-    @Override
-    public void onBindViewHolder(BrowseHolder holder, int position) {
-        BrowseItem item = browseItem.get(position);
-        //if((tracker%2)==0)
-        //{
-            holder.title.setText(item.getTitle());
-            //holder.title2.setText(item.getTitle2());
-            holder.icon.setImageResource(item.getImageResID());
-            //holder.icon2.setImageResource(item.getImageResID2());
-        //}
-        //else
-        //{
-            //holder.title.setText(item.getTitle());
-            holder.title2.setText(item.getTitle2());
-            //holder.icon.setImageResource(item.getImageResID());
-            holder.icon2.setImageResource(item.getImageResID2());
-        //}
 
+    @Override
+    public void onBindViewHolder(BrowseHolder holder, final int position) {
+        BrowseItem item = browseItem.get(position);
+
+//        holder.itemView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Snackbar.make(v.findViewById(R.id.browseitem), "clicked" + position, Snackbar.LENGTH_SHORT).show();
+//            }
+//        });
+
+        holder.title.setText(item.getTitle());
+        holder.icon.setImageResource(item.getImageResID());
+
+        holder.title2.setText(item.getTitle2());
+        holder.icon2.setImageResource(item.getImageResID2());
 
     }
 
--- app/src/main/java/com/example/thepo/BrowseStations.java
+++ app/src/main/java/com/example/thepo/BrowseStations.java
@@ -14,45 +14,45 @@ import android.widget.TextView;
 
 public class BrowseStations extends AppCompatActivity {
 
-    private RecyclerView recView;
-    private BrowseAdapter adapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        recView = (RecyclerView)findViewById(R.id.browseactivity);
-        //LayoutManager: Linear Layout or Grid or Staggered grid
-        recView.setLayoutManager(new LinearLayoutManager(this));
-
-        adapter = new BrowseAdapter(MockData.getBrowseData(), this);
-        recView.setAdapter(adapter);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-    public void clickMeCard(View v)
-    {
-        Intent i = new Intent(BrowseStations.this, Tab2Radio.class);
-        startActivity(i);
-    }
-    public void radioButtonClick(MenuItem v)
-    {
-        Intent i = new Intent(BrowseStations.this, Tab2Radio.class);
-        startActivity(i);
-    }
-    public void uploadButtonClick(MenuItem v)
-    {
-        Intent i = new Intent(BrowseStations.this, Tab3Upload.class);
-        startActivity(i);
-    }
-    public void browseButtonClick(MenuItem v)
-    {
-        Intent i = new Intent(BrowseStations.this, MainActivity.class);
-        startActivity(i);
-    }
+//    private RecyclerView recView;
+//    private BrowseAdapter adapter;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        recView = (RecyclerView)findViewById(R.id.browseactivity);
+//        //LayoutManager: Linear Layout or Grid or Staggered grid
+//        recView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        adapter = new BrowseAdapter(MockData.getBrowseData(), this);
+//        recView.setAdapter(adapter);
+//    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//    public void clickMeCard(View v)
+//    {
+//        Intent i = new Intent(BrowseStations.this, Tab2Radio.class);
+//        startActivity(i);
+//    }
+//    public void radioButtonClick(MenuItem v)
+//    {
+//        Intent i = new Intent(BrowseStations.this, Tab2Radio.class);
+//        startActivity(i);
+//    }
+//    public void uploadButtonClick(MenuItem v)
+//    {
+//        Intent i = new Intent(BrowseStations.this, Tab3Upload.class);
+//        startActivity(i);
+//    }
+//    public void browseButtonClick(MenuItem v)
+//    {
+//        Intent i = new Intent(BrowseStations.this, MainActivity.class);
+//        startActivity(i);
+//    }
 
 }
