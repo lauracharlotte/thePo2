4e5138362d7c61c1b95a92931df47c22d740aa4b
lauracharlotte <laura.c.hovey@gmail.com>
2017-04-17 21:29:51 -0600

    created activities, made pause button, songs start as soon as you open page

---
+++ app/src/main/java/com/example/thepo/BlueGrassRadio.java
@@ -0,0 +1,13 @@
+package com.example.thepo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlueGrassRadio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blue_grass_radio);
+    }
+}
